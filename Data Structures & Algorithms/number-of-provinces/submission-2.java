7d1ed01b3b0c96f1a5208ee83252300b1943f9f7
Vidushi-Dwivedi <96303323+Vidushi-Dwivedi@users.noreply.github.com>
2026-04-16 01:17:41 +0530

    Add: number-of-provinces - submission-2

---
+++ Data Structures & Algorithms/number-of-provinces/submission-2.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int find(int x, int[] parent){
+        if(parent[x] != x){
+            parent[x] = find(parent[x], parent);
+        }
+
+        return parent[x];
+    }
+
+    public void union(int u, int v, int[] parent, int[] rank){
+        int pu = find(u, parent);
+        int pv = find(v, parent);
+        if(pu != pv){
+            if(rank[pu] > rank[pv]){
+                parent[pv] = pu;
+                rank[pu]++;
+            } else {
+                parent[pu] = pv;
+                rank[pv]++;
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int[] rank = new int[isConnected.length];
+        int[] parent = new int[isConnected.length];
+        List<List<Integer>> edges = new ArrayList<>();
+
+        for(int i = 0; i< isConnected.length; i++){
+            parent[i] = i;
+            for(int j = 0; j<isConnected[0].length; j++){
+                if(isConnected[i][j] == 1){
+                    edges.add(new ArrayList<>(Arrays.asList(i, j)));
+                }
+            }
+        }
+
+        for(List<Integer> edge: edges){
+            union(edge.get(0), edge.get(1), parent, rank);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i< parent.length; i++){
+            int x = find(parent[i], parent);
+
+            set.add(x);
+        }
+
+        return set.size();
+    }
+}
