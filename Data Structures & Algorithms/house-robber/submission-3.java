b3ffbac5cb3f1631c32a0fb64af4615d0bcb72db
Vidushi-Dwivedi <96303323+Vidushi-Dwivedi@users.noreply.github.com>
2026-04-22 00:38:56 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int memo(int[] nums, int n, int[] dp){
+        System.out.println(n);
+        if(n < 0){ return 0;}
+        if(n==0){ return dp[0] = nums[0]; }
+        
+        if(dp[n] == -1){
+            int inc = memo(nums, n - 2, dp) + nums[n];
+            int exc = memo(nums, n-1, dp);
+            dp[n] = Math.max(inc, exc);
+        }
+
+        return dp[n];
+    }
+    // public tab(int[] nums){
+    //     int n = nums.length;
+
+    // }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        memo(nums, n - 1, dp);
+
+        return dp[n - 1];
+    }
+}
