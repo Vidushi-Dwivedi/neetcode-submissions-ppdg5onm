8e2ef87b0f55383170d848f9b9ff92d02c8a0768
Vidushi-Dwivedi <96303323+Vidushi-Dwivedi@users.noreply.github.com>
2026-04-30 19:29:58 +0530

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.java
@@ -0,0 +1,92 @@
+class Solution {
+    public int opt(String t1, String t2){
+        int l1 = t1.length();
+        int l2 = t2.length();
+
+        int[] dp = new int[l2 + 1];
+        int diagonal;
+
+        for(int i = 1; i<= l1; i++){
+            diagonal = 0;
+            for(int j = 1; j <= l2; j++){
+                int temp = dp[j];
+                if(t1.charAt(i - 1) == t2.charAt(j - 1)){
+                    dp[j] = 1 + diagonal;
+                } else{ 
+                    dp[j] = Math.max(dp[j-1], dp[j]);
+                }
+                diagonal = temp;
+            }
+        }
+
+        return dp[l2];
+    }
+
+    public int tab(String t1, String t2){
+        int l1 = t1.length();
+        int l2 = t2.length();
+
+        int[][] dp = new int[l1 + 1][l2 + 1];
+
+        for(int i = 1; i<= l1; i++){
+            for(int j = 1; j <= l2; j++){
+                if(t1.charAt(i - 1) == t2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i -1][j - 1];
+                    continue;
+                } 
+
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[l1][l2];
+    }
+
+    public int memo(String t1, String t2, int i, int j, int[][] dp) {
+        if (i >= t1.length() || j >= t2.length()) {
+            return 0;
+        }
+
+        if (dp[i][j] == -1) {
+            if (t1.charAt(i) == t2.charAt(j)) {
+                return dp[i][j] = 1 + memo(t1, t2, i + 1, j + 1, dp);
+            }
+
+            int a = memo(t1, t2, i + 1, j, dp);
+            int b = memo(t1, t2, i, j + 1, dp);
+
+            dp[i][j] = Math.max(a, b);
+        }
+
+        return dp[i][j];
+    }
+
+    public int helper(String t1, String t2, int i, int j) {
+        if (i >= t1.length() || j >= t2.length()) {
+            return 0;
+        }
+
+        if (t1.charAt(i) == t2.charAt(j)) {
+            return 1 + helper(t1, t2, i + 1, j + 1);
+        }
+
+        int a = helper(t1, t2, i + 1, j);
+        int b = helper(t1, t2, i, j + 1);
+
+        return Math.max(a, b);
+    }
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        // return helper(text1, text2, 0, 0);
+
+        // int[][] dp = new int[text1.length()][text2.length()];
+
+        // for (int i = 0; i < text1.length(); i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
+
+        // return memo(text1, text2, 0, 0, dp);
+
+        return opt(text1, text2);
+    }
+}
