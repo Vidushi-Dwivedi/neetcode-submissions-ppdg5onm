a26f837abfcb3e6e89e350ff40cd0a0486852f1d
Vidushi-Dwivedi <96303323+Vidushi-Dwivedi@users.noreply.github.com>
2026-05-01 18:11:12 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int tab(int amount, int[] coins){
+        int n =coins.length;
+        int[][] dp = new int[amount + 1][n + 1];
+
+        for(int i = 0; i<= n; i++){
+            dp[0][i] = 1;
+        }
+
+        for(int i = 0; i<=amount; i++){
+            for(int j = n - 1; j>= 0; j--){
+                dp[i][j] = (i-coins[j] >= 0) ? dp[i - coins[j]][j] : 0;
+                dp[i][j] += dp[i][j + 1];
+            }
+        }
+
+        return dp[amount][0];
+    }
+
+    public int memo(int amount, int[] coins, int i, int[][] dp){
+        if(amount == 0){
+            return 1;
+        } 
+        if(i>= coins.length || amount < 0){
+            return 0;
+        }
+
+        if(dp[amount][i] != -1){
+            return dp[amount][i];
+        }
+
+        int include = memo(amount - coins[i], coins, i, dp);
+        int exclude = memo(amount, coins, i + 1, dp);
+        dp[amount][i] = include + exclude;
+
+        return dp[amount][i];
+    }
+
+    public int helper(int amount, int[] coins, int i){
+        if(amount == 0){
+            return 1;
+        } 
+        if(i>= coins.length || amount < 0){
+            return 0;
+        }
+
+        int ways = 0;
+        int include = helper(amount - coins[i], coins, i);
+        int exclude = helper(amount, coins, i + 1);
+        ways += include + exclude;
+
+        return ways;
+    }
+    public int change(int amount, int[] coins) {
+        // return helper(amount, coins, 0);
+        // int[][] dp = new int[amount + 1][coins.length];
+
+        // for(int i = 0; i<= amount; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return memo(amount, coins, 0, dp);
+        return tab(amount, coins);
+    }
+}
