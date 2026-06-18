0e02e27795a1cfaa56734248b89a2b95d3a17dd3
Vidushi-Dwivedi <96303323+Vidushi-Dwivedi@users.noreply.github.com>
2026-06-19 02:02:40 +0530

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.java
@@ -0,0 +1,50 @@
+class DSU{
+        int[] parent, rank;
+        int components;
+
+        DSU(int n){
+            parent = new int[n];
+            rank = new int[n];
+            components = n;
+
+            for(int i = 0; i< n; i++){
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x){
+            if(parent[x] != x){
+                parent[x] = find(parent[x]);
+            }
+
+            return parent[x];
+        }
+
+        public void union(int a, int b){
+            int pa = find(a);
+            int pb = find(b);
+
+            if(pa == pb){ return;}
+
+            if(rank[pa] > rank[pb]){ parent[pb] = pa; }
+            else if( rank[pb] > rank[pa]){ parent[pa] = pb;}
+            else{
+                parent[pa] = pb;
+                rank[pb]++;
+            }
+
+            components--;
+        }
+    }
+
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        DSU dsu = new DSU(n);
+
+        for(int[] e: edges){
+            dsu.union(e[0], e[1]);
+        }
+
+        return dsu.components;
+    }
+}
